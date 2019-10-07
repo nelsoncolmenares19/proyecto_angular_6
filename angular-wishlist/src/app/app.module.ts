88e8ed42634bf864e8c2272626fc15fcb32f8ae0
ncolmenares <nelsoncolmenares19@gmail.com>
2019-10-07 16:53:19 -0400

    incorporacion del ruteo dentro de la app

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+
+const routes:Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home', component: ListaDestinosComponent},
+  {path:'destino', component: ListaDestinosComponent}
+];
 
 @NgModule({
   declarations: [
