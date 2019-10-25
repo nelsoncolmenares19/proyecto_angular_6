893d3349c1c2555d8f0b0756b2ef129aebb7ad1c
ncolmenares <nelsoncolmenares19@gmail.com>
2019-10-25 19:51:29 -0400

    componentes de geolocalizacion, animaciones

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -6,7 +6,9 @@ import { StoreModule as NgRxStoreModule, ActionReducerMap, Store } from '@ngrx/s
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import Dexie from 'dexie';
-import { TranslateService, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './components/destino-viaje/destino-viaje.component';
@@ -207,7 +209,9 @@ function HttpLoaderFactory(http: HttpClient) {
           useFactory: (HttpLoaderFactory),
           deps: [HttpClient]
       }
-    })
+    }),
+    NgxMapboxGLModule,
+    BrowserAnimationsModule
   ],
   // providers: [],
   providers: [
--- angular-wishlist/src/app/components/destino-detalle/destino-detalle.component.ts
+++ angular-wishlist/src/app/components/destino-detalle/destino-detalle.component.ts
@@ -9,15 +9,33 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./destino-detalle.component.css'],
   providers: [DestinosApiClient]
 })
+
 export class DestinoDetalleComponent implements OnInit {
   destino: DestinoViaje;
+  style = {
+    sources: {
+      world: {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json'
+      }
+    },
+    version: 8,
+    layers: [{
+      'id': 'countries',
+      'type': 'fill',
+      'source': 'world',
+      'layout': {},
+      'paint': {
+        'fill-color': '#6F788A'
+      }
+    }]
+  };
 
-  constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClient) { }
+  constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClient) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
-    this.destino = null;
-    // this.destino = this.destinosApiClient.getById(id);
+    this.destino = this.destinosApiClient.getById(id);
   }
 
 }
